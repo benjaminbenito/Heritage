d3beb8ce6981d60f40965bdd9d6671d37e104324
Benjamin <Benjamin.benito@orange.fr>
2020-07-20 15:09:03 +0200

    Create Class Carre.

---
+++ Heritage/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Heritage
+{
+    internal class Rectangle : Forme
+    {
+        public int Largeur { get; set; }
+        public int Longueur { get; set; }
+
+        public override double Aire => Largeur*Longueur;
+
+        public override double Perimetre => (Largeur+Longueur)*2;
+
+        public override string ToString()
+        {
+            return $"Rectangle de Longueur={Longueur} et de Largeur={Largeur}"  + Environment.NewLine + base.ToString();
+        }
+    }
+}
---
+++ Heritage/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Heritage
+{
+    internal class Triangle : Forme
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public override double Aire => (A*B)/2;
+
+        public override double Perimetre => A*B*C;
+
+        public override string ToString()
+        {
+            return $"Triangle de cote A={A}, B={B}, C={C} " + Environment.NewLine + base.ToString();
+        }
+    }
+}
