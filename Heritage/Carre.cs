62f36e959a50ad6a3ef42bafb058db80b527c778
Benjamin <Benjamin.benito@orange.fr>
2020-07-20 15:13:14 +0200

    Create Class Carre.

---
+++ Heritage/Carre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Heritage
+{
+    internal class Carre : Forme
+    {
+        public int Longueur { get; set; }
+
+        public override double Aire => Longueur*Longueur;
+
+        public override double Perimetre => Longueur*4;
+
+        public override string ToString()
+        {
+            return $"Carre de cote {Longueur}" + Environment.NewLine + base.ToString();
+        }
+    }
+}
